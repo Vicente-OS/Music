6aee800a6e502eacaf80f1413746006a596594fe
Vicente-OS <82908909+Vicente-OS@users.noreply.github.com>
2021-10-19 10:07:14 -0500

    third commit

--- script.js
+++ script.js
@@ -71,7 +71,17 @@ function nextSong() {
 // Update progress bar
 function updateProgress(e) {
     const { duration, currentTime} = e.srcElement;
-    const progressPercent 
+    const progressPercent = (currentTime /duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX /width) * duration;
 }
 
 // Event listeners
@@ -92,3 +102,9 @@ nextBtn.addEventListener('click', nextSong);
 
 // Time/song update
 audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
